e7f23fd02cc9050cf7128a9e7fbed79123013a5c
jesfuen <jesusdelafuente2002@gmail.com>
2026-04-20 17:31:55 +0200

    crear funciones basicas de UserDAO y Base de Datos de SmashFeed

---
+++ app/src/main/java/com/example/smashfeed/data/local/SmashFeedRoomDatabase.kt
@@ -0,0 +1,6 @@
+package com.example.smashfeed.data.local
+
+
+class SmashFeedRoomDatabase {
+
+}
--- app/src/main/java/com/example/smashfeed/data/local/dao/UserDAO.kt
+++ app/src/main/java/com/example/smashfeed/data/local/dao/UserDAO.kt
@@ -1,5 +1,21 @@
 package com.example.smashfeed.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.smashfeed.data.local.entity.UserEntity
+
+@Dao
 interface UserDAO {
+    @Insert
+    fun insert(vararg userEntity: UserEntity)
+
+    @Update
+    fun update(userEntity: UserEntity)
+
+    @Delete
+    fun delete(userEntity: UserEntity)
 
 }
