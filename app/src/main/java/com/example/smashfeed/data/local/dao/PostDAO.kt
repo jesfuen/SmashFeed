478fa81dde136895c45d824808edf1ab2b8f9c8a
jesfuen <jesusdelafuente2002@gmail.com>
2026-04-21 00:03:58 +0200

    implementar PostRepository y añadir query getAllPosts a PostDAO

--- app/src/main/java/com/example/smashfeed/data/local/dao/PostDAO.kt
+++ app/src/main/java/com/example/smashfeed/data/local/dao/PostDAO.kt
@@ -1,8 +1,10 @@
 package com.example.smashfeed.data.local.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.example.smashfeed.data.local.entity.PostEntity
 
@@ -18,4 +20,6 @@ interface PostDAO {
     fun delete(postEntity: PostEntity)
 
     // Querys de Post
+    @Query("SELECT * FROM post_table")
+    fun getAllPosts(): LiveData<List<PostEntity>>
 }
--- app/src/main/java/com/example/smashfeed/data/repository/PostRepository.kt
+++ app/src/main/java/com/example/smashfeed/data/repository/PostRepository.kt
@@ -1,4 +1,30 @@
 package com.example.smashfeed.data.repository
 
-class PostRepository {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
+import com.example.smashfeed.data.local.dao.PostDAO
+import com.example.smashfeed.data.local.mapper.toDomain
+import com.example.smashfeed.data.local.mapper.toEntity
+import com.example.smashfeed.data.model.Post
+
+class PostRepository(
+    private val postDAO: PostDAO
+){
+    fun getAllPosts(): LiveData<List<Post>> {
+        return postDAO.getAllPosts().map { listPosts ->
+            listPosts.map { it.toDomain() }
+        }
+    }
+
+    fun addPost(post: Post) {
+        postDAO.insert(post.toEntity())
+    }
+
+    fun updatePost(post: Post) {
+        postDAO.update(post.toEntity())
+    }
+
+    fun deletePost(post: Post) {
+        postDAO.delete(post.toEntity())
+    }
 }
