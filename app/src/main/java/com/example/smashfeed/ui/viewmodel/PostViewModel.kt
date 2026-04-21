0790027446da5f010603863fe9d3005daf10c4d1
jesfuen <jesusdelafuente2002@gmail.com>
2026-04-21 20:16:40 +0200

    Implementar PostViewModel.kt

--- app/src/main/java/com/example/smashfeed/ui/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/example/smashfeed/ui/viewmodel/PostViewModel.kt
@@ -1,4 +1,40 @@
 package com.example.smashfeed.ui.viewmodel
 
-class PostViewModel {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.smashfeed.data.local.SmashFeedRoomDatabase
+import com.example.smashfeed.data.model.Post
+import com.example.smashfeed.data.repository.PostRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class PostViewModel(context: Context): ViewModel() {
+    private val postRepository: PostRepository
+    val posts: LiveData<List<Post>>
+
+    init {
+        val postDao = SmashFeedRoomDatabase.getInstance(context).postDAO()
+        postRepository = PostRepository(postDao)
+        posts = postRepository.getAllPosts()
+    }
+
+    fun addPost(post: Post) {
+        viewModelScope.launch(Dispatchers.IO) {
+            postRepository.addPost(post)
+        }
+    }
+
+    fun updatePost(post: Post) {
+        viewModelScope.launch(Dispatchers.IO) {
+            postRepository.updatePost(post)
+        }
+    }
+
+    fun deletePost(post: Post) {
+        viewModelScope.launch(Dispatchers.IO) {
+            postRepository.deletePost(post)
+        }
+    }
 }
